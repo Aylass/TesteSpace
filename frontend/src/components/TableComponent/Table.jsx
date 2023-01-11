9183a21944b9e9081332d0461ce6c12fd3bfe6e3
Aylass <42043448+Aylass@users.noreply.github.com>
2023-01-11 14:01:02 -0300

    Criado modal, e adicionado função de fecha-lo

--- frontend/src/components/TableComponent/Table.jsx
+++ frontend/src/components/TableComponent/Table.jsx
@@ -26,70 +26,114 @@ class Table extends React.Component{
 
     render(){
         return(
-            <table>
-                {this.buildHeader()}
-                <tbody>
-                    {this.props.dataRow.map?.(user => {
-                        //user -------------------------------------------------------
-                        let userName = "";
-                        let userId = "";
-                        let userDate = "";
-                        let userSalary = "";
-                        let userCar = "";
-                        let userStatus = "";
+            <>
+                <EditModal/>
+                <table>
+                    {this.buildHeader()}
+                    <tbody>
+                        {this.props.dataRow.map?.(user => {
+                            //user -------------------------------------------------------
+                            let userName = "";
+                            let userId = "";
+                            let userDate = "";
+                            let userSalary = "";
+                            let userCar = "";
+                            let userStatus = "";
 
-                        if(user.user_first_name){
-                            userName = user.user_first_name;
-                        }
-                        if(user.user_birth_date){
-                            userDate = user.user_birth_date;
-                        }
-                        if(user.user_job_id){
-                            userSalary = user.user_job_id;
-                        }
-                        if(user.user_car_id){
-                            userCar = user.user_car_id;
-                        }
-                        if(user.user_access_id){
-                            userId = user.user_access_id;
-                        }
-                        userStatus = user.status;
+                            if(user.user_first_name){
+                                userName = user.user_first_name;
+                            }
+                            if(user.user_birth_date){
+                                userDate = user.user_birth_date;
+                            }
+                            if(user.user_job_id){
+                                userSalary = user.user_job_id;
+                            }
+                            if(user.user_car_id){
+                                userCar = user.user_car_id;
+                            }
+                            if(user.user_access_id){
+                                userId = user.user_access_id;
+                            }
+                            userStatus = user.status;
 
-                        //user cars-------------------------------------------------------
-                        const userCurrentCar = this.props.dataCars[userCar];
-                        
-                        //user emprego-------------------------------------------------------
-                        const userCurrentJob = this.props.dataJobs[userSalary];
-                        
-                        //user produtos-------------------------------------------------------
-                        const userCurrentProduct = this.props.dataProducts[userSalary];
-                        
-                        //user Acessos-------------------------------------------------------
-                        const userCurrentAccess = this.props.dataAccess[userId];
-                        
-                        //user Endereços-------------------------------------------------------
-                        const userCurrentAddresses = this.props.dataAddresses[userId];
-        
-                     return(<Item 
-                            key={`item_${user.user_id}`} 
-                            name={userName} 
-                            date={userDate} 
-                            salary={userCurrentJob?.user_job_salary || ""} 
-                            car={userCar} 
-                            status={userStatus} 
-                            user={user}
-                            currentCar={userCurrentCar}
-                            currentJob={userCurrentJob}
-                            currentProduct={userCurrentProduct}
-                            currentAccess={userCurrentAccess}
-                            currentAdresses={userCurrentAddresses}
-                        />)})
-                    }
-                </tbody>
-            </table>
+                            //user cars-------------------------------------------------------
+                            const userCurrentCar = this.props.dataCars[userCar];
+                            
+                            //user emprego-------------------------------------------------------
+                            const userCurrentJob = this.props.dataJobs[userSalary];
+                            
+                            //user produtos-------------------------------------------------------
+                            const userCurrentProduct = this.props.dataProducts[userSalary];
+                            
+                            //user Acessos-------------------------------------------------------
+                            const userCurrentAccess = this.props.dataAccess[userId];
+                            
+                            //user Endereços-------------------------------------------------------
+                            const userCurrentAddresses = this.props.dataAddresses[userId];
+            
+                        return(<Item 
+                                key={`item_${user.user_id}`} 
+                                name={userName} 
+                                date={userDate} 
+                                salary={userCurrentJob?.user_job_salary || ""} 
+                                car={userCar} 
+                                status={userStatus} 
+                                user={user}
+                                currentCar={userCurrentCar}
+                                currentJob={userCurrentJob}
+                                currentProduct={userCurrentProduct}
+                                currentAccess={userCurrentAccess}
+                                currentAdresses={userCurrentAddresses}
+                            />)})
+                        }
+                    </tbody>
+                </table>
+            </>
         )
     }
 
+}
+
+class EditModal extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    closeModal(){
+        var modal = document.getElementById("editModal");
+        modal.style.display = "none";
+    }
+    
+    render(){
+        window.onclick = function(event) {
+            var modal = document.getElementById("editModal");
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+          }
+        
+        return(
+            <>
+                <div id="editModal" className="modal">
+                    
+                    <div className="modal-content">
+                        <button className="closeModalButton" onClick={this.closeModal}>X</button>
+                        <p className="titulo">Titulo</p>
+                        <hr/>
+                        <p>dskoadnaodnklfndjfnjdakfnjkfndjkafk</p>
+                        <button className="editModalButton" onClick={this.closeModal}>Editar</button>
+                        <button className="saveModalButton" onClick={this.closeModal}>Salvar</button>
+                        <button className="cancelModalButton" onClick={this.closeModal}>Cancelar</button>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 class Item extends React.Component{
@@ -100,10 +144,18 @@ class Item extends React.Component{
          isOpemItem : false,
         }
         this.toggleBodyItem = this.toggleBodyItem.bind(this);
+        this.openEditModal = this.openEditModal.bind(this);
     }
 
     toggleBodyItem(){
         this.setState({isOpemItem : !this.state.isOpemItem});
+    }
+
+    openEditModal(){
+        console.log("oi")
+        // Get the modal
+        var modal = document.getElementById("editModal");
+        modal.style.display = "block";
     }
 
     render(){
@@ -115,7 +167,7 @@ class Item extends React.Component{
                     <td>{this.props.name}</td>
                     <td>{this.props.date}</td>
                     <td>{this.props.salary}</td>
-                    <td>{this.props.car}</td>
+                    <td><button onClick={this.openEditModal}>Visualizar</button></td>
                     <td>{this.props.status === true?
                         <FontAwesomeIcon style={{color : '#20B2AA'}} icon={faUser} /> : 
                         this.props.status === false?
