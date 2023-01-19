6b3c77181d0123f367875948a087ba437e804573
Aylass <42043448+Aylass@users.noreply.github.com>
2023-01-19 16:54:13 -0300

    Organização de renderização do componente Input.

--- frontend/src/components/InputComponent/Input.jsx
+++ frontend/src/components/InputComponent/Input.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 class InputComponent extends React.Component{
 
+    shouldComponentUpdate(nextProps){
+        if(nextProps.disabled !== this.props.disabled){
+            return true;
+        }
+        if(nextProps.data !== this.props.data){
+            return true;
+        }
+        return false;
+    }
+
     render(){
         return(
-            <input className="input" type="text" placeholder={this.props.data} name={this.props.data} onChange={this.props.onChange} value={this.props.data} disabled={this.props.disabled}  />
+            <input 
+                className="input" 
+                type="text" 
+                placeholder={this.props.data} 
+                name={this.props.data} 
+                onChange={this.props.onChange} 
+                value={this.props.data} 
+                disabled={this.props.disabled}  
+            />
         )
     }
 }
