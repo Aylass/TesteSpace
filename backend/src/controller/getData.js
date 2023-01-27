9b5d570426ca8a676a909293acfbce73dff47cfb
Aylass <42043448+Aylass@users.noreply.github.com>
2023-01-27 16:31:10 -0300

    Pegando dados da tabela de users_address do banco.

--- backend/src/controller/getData.js
+++ backend/src/controller/getData.js
@@ -2,6 +2,7 @@ const getUsersModel = require('../model/getUsers.js');
 const getCarModel = require('../model/getCar.js');
 const getJobModel = require('../model/getJob.js');
 const getAccessModel = require('../model/getAccess.js');
+const getAddressModel = require('../model/getAddress.js');
 
 exports.getData = async (req, res) => {
     //let id = req.params.id;
@@ -10,9 +11,10 @@ exports.getData = async (req, res) => {
     const car = await getCarModel.car();
     const job = await getJobModel.job();
     const access = await getAccessModel.access();
+    const address = await getAddressModel.address();
 
     const response = {
-        user, car, job, access
+        user, car, job, access, address
     }
     
     res.json(response)
