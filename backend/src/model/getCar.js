8fd182e9acf08cdb90fdf8b2ff32ba8f23032cb6
Aylass <42043448+Aylass@users.noreply.github.com>
2023-02-02 10:42:57 -0300

    Criando log para erros em getCar.

--- backend/src/model/getCar.js
+++ backend/src/model/getCar.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+var fs = require('fs');
 
 exports.car = function getCar() {
  
@@ -26,10 +27,28 @@ exports.car = function getCar() {
         return client
             .query(query)
             .then(successful)
-            .catch(err => console.log(err));
+            .catch((error) => {
+                const date = new Date();
+                fs.appendFile(`./logs/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-getUsers.txt`,
+                error.toString(),
+                    function (err) {
+                        if (err) throw err;
+                        console.log('Saved!');
+                    }
+                );
+            });
     }
 
     return client.connect()
         .then(executeQuery)
-        .catch(err => console.log(err))
+        .catch((error) => {
+            const date = new Date();
+            fs.appendFile(`./logs/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-getUsers.txt`,
+            error.toString(),
+                function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                }
+            );
+        });
 }
